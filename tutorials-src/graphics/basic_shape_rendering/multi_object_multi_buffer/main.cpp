d3cd2c218c760236abaffaf3dc11e794b39300bd
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-11-24 16:27:57 -0500

    Add CMakeLists for some projects

--- tutorials-src/graphics/basic_shape_rendering/multi_object_multi_buffer/main.cpp
+++ tutorials-src/graphics/basic_shape_rendering/multi_object_multi_buffer/main.cpp
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
 
 GLFWwindow* window;
 
@@ -45,33 +46,61 @@ GLfloat verticesB[] = {
 	+0.3f, 0.0f
 };
 
-const char* vertexSource =
-"#version 150\n"
-"in vec2 position;"
-"void main()"
-"{"
-"gl_Position = vec4(position, 0.0, 1.0);"
-"}";
-
-const char* fragmentSource =
-"#version 150\n"
-"out vec4 outColor;"
-"void main()"
-"{"
-"outColor = vec4(1.0, 1.0, 1.0, 1.0);"
-"}";
-
-
-void initShaders()
+// This method reads the text from a file.
+// Realistically, we wouldn't want plain text shaders hardcoded in, we'd rather read them in from a separate file so that the shader code is separated.
+std::string readShader(std::string fileName)
 {
+	std::string shaderCode;
+	std::string line;
+
+	// We choose ifstream and std::ios::in because we are opening the file for input into our program.
+	// If we were writing to the file, we would use ofstream and std::ios::out.
+	std::ifstream file(fileName, std::ios::in);
+
+	// This checks to make sure that we didn't encounter any errors when getting the file.
+	if (!file.good())
+	{
+		std::cout << "Can't read file: " << fileName.data() << std::endl;
+
+		// Return so we don't error out.
+		return "";
+	}
+
+	// ifstream keeps an internal "get" position determining the location of the element to be read next
+	// seekg allows you to modify this location, and tellg allows you to get this location
+	// This location is stored as a streampos member type, and the parameters passed in must be of this type as well
+	// seekg parameters are (offset, direction) or you can just use an absolute (position).
+	// The offset parameter is of the type streamoff, and the direction is of the type seekdir (an enum which can be ios::beg, ios::cur, or ios::end referring to the beginning, 
+	// current position, or end of the stream).
+	file.seekg(0, std::ios::end);					// Moves the "get" position to the end of the file.
+	shaderCode.resize((unsigned int)file.tellg());	// Resizes the shaderCode string to the size of the file being read, given that tellg will give the current "get" which is at the end of the file.
+	file.seekg(0, std::ios::beg);					// Moves the "get" position to the start of the file.
+
+	// File streams contain two member functions for reading and writing binary data (read, write). The read function belongs to ifstream, and the write function belongs to ofstream.
+	// The parameters are (memoryBlock, size) where memoryBlock is of type char* and represents the address of an array of bytes are to be read from/written to.
+	// The size parameter is an integer that determines the number of characters to be read/written from/to the memory block.
+	file.read(&shaderCode[0], shaderCode.size());	// Reads from the file (starting at the "get" position which is currently at the start of the file) and writes that data to the beginning
+	// of the shaderCode variable, up until the full size of shaderCode. This is done with binary data, which is why we must ensure that the sizes are all correct.
+
+	file.close(); // Now that we're done, close the file and return the shaderCode.
+
+	return shaderCode;
+}
+void initShaders(std::string vert, std::string frag)
+{
+	const char *vert_txt = vert.c_str();
+	const char *frag_txt = frag.c_str();
+	const int vert_size = vert.size();
+	const int frag_size = frag.size();
+	std::cout << vert_txt << std::endl;
 	// Compile the vertex shader
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glShaderSource(vertexShader, 1, &vert_txt, &vert_size);
 	glCompileShader(vertexShader);
 
-	// Compile the fragment shader
+	// Compile the fragment shaderfrag_size
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glShaderSource(fragmentShader, 1, &frag_txt, &frag_size);
 	glCompileShader(fragmentShader);
 
 	// Combine shaders into a program, bind, link and use
@@ -121,7 +150,7 @@ int init()
 	glBindBuffer(GL_ARRAY_BUFFER, triangleBufferB);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesB), verticesB, GL_STATIC_DRAW);
 
-	initShaders();
+	initShaders(readShader("VertexShader.glsl"), readShader("FragmentShader.glsl"));
 
     return 0;
 }
